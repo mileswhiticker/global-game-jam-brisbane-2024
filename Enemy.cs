d9031156791f348d7e9e96adbab43e85807f9d47
Miles Whiticker <miles.whiticker@gmail.com>
2024-01-27 03:22:03 +1000

    add basic mob spawning and enemy AI, manual merges some naming conflicts, adds world scene
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

---
+++ Enemy.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class Enemy : CharacterBody2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void MoveTo(Vector2 newPos)
+	{
+        GD.Print("Enemy::MoveTo(" + newPos.X + "," + newPos.Y + ")");
+        Position = newPos;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		var velocity = Velocity;
+		velocity.X += 10.0f;
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
---
+++ mob_spawner.cs
@@ -0,0 +1,50 @@
+using Godot;
+using System;
+using System.Text;
+
+public partial class mob_spawner : Node
+{
+	[Export]
+	public PackedScene EnemyScene;
+    [Export]
+    public double tLeftSpawnEnemy = 1.0f;
+    [Export]
+    public double enemySpawnInterval = 3.0f;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		tLeftSpawnEnemy -= delta;
+		if(tLeftSpawnEnemy < 0)
+        {
+            GD.Print("spawning enemy");
+            tLeftSpawnEnemy = enemySpawnInterval;
+			SpawnEnemy();
+        }
+		//GD.Print(tLeftSpawnEnemy);
+    }
+
+	protected void SpawnEnemy()
+    {
+        if (EnemyScene != null)
+        {
+            Enemy mob = (Enemy)EnemyScene.Instantiate();
+            AddChild(mob);
+            Vector2 spawnPos = GetNextSpawnPos();
+            mob.MoveTo(spawnPos);
+        }
+    }
+
+    protected Vector2 GetNextSpawnPos()
+    {
+        Vector2 dimsLower = new Vector2(100, 100);
+        Vector2 dimsUpper = new Vector2(100, 600);
+        Vector2 spawnPos = new Vector2((float)GD.RandRange(dimsLower.X, dimsUpper.X), (float)GD.RandRange(dimsLower.Y, dimsUpper.Y));
+        return spawnPos;
+    }
+}
