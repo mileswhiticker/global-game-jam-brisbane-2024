d4188ffa2cdb1a72c2e5593223516e979a205f46
Simone Rizzuto <simorico.dev@gmail.com>
2024-01-26 21:31:55 +1000

    Added player movement

--- Player.cs
+++ Player.cs
@@ -1,15 +1,44 @@
 using Godot;
 using System;
+using globalgamejam2024.Shared;
 
 public partial class Player : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export] public int Speed { get; set; } = 500;
+	
 	public override void _Ready()
 	{
+		
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var translatedVector = GetMovementVector();
+		translatedVector = translatedVector.Normalized();
+		translatedVector *= Speed * (float)delta;
+		
+		Transform = Transform.Translated(translatedVector);
+	}
+
+	private Vector2 GetMovementVector()
+	{
+		var translatedVector = Vector2.Zero;
+		
+		if (Input.IsActionPressed(MovementDirection.Up))
+		{
+			translatedVector += Vector2.Up;
+		}
+		if (Input.IsActionPressed(MovementDirection.Down))
+		{
+			translatedVector += Vector2.Down;
+		}
+		if (Input.IsActionPressed(MovementDirection.Left))
+		{
+			translatedVector += Vector2.Left;
+		}
+		if (Input.IsActionPressed(MovementDirection.Right))
+		{
+			translatedVector += Vector2.Right;
+		}
 	}
 }
