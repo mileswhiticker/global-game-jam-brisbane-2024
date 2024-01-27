93663d25b4c1d6faab335fe9f35431864f3f5cd7
Simone Rizzuto <simorico.dev@gmail.com>
2024-01-27 23:24:40 +1000

    Added UI

--- scenes/UI.cs
+++ scenes/UI.cs
@@ -1,16 +1,29 @@
 using Godot;
-using System;
-
 public partial class UI : Node
 {
+	[Export] public TextureProgressBar? ProgressBar;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		if (ProgressBar == null) return;
 		
+		
+		GD.Print(ProgressBar.Name);		
+		GD.Print(ProgressBar.Value);		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
+	}
+
+	public double TakeDamage()
+	{
+		if (ProgressBar == null) return 0;
+
+		ProgressBar.Value -= 1;
+		return ProgressBar.Value;
 	}
 }
